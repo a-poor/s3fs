3d6860a919f97b3a57945bddd642402412e14103
a-poor <45295232+a-poor@users.noreply.github.com>
2022-02-06 10:36:09 -0800

    Updating `symlink.go`
    Setting all methods to (currently) return an error showing that
    symlinks aren't supported by S3. This may be re-evaluated later
    for commands like `Lstat` or `ReadLink` which don't necessarily
    require symlinks to perform.

--- symlink.go
+++ symlink.go
@@ -14,18 +14,26 @@ var (
 // Lstat returns a FileInfo describing the named file. If the file is a
 // symbolic link, the returned FileInfo describes the symbolic link. Lstat
 // makes no attempt to follow the link.
+//
+// NOTE: Lstat is not supported by s3. It always returns an error.
+// (This may be revised in the future.)
 func (fs *S3FS) Lstat(filename string) (os.FileInfo, error) {
-	return nil, nil
+	return nil, ErrSymLinkNotSupported
 }
 
 // Symlink creates a symbolic-link from link to target. target may be an
 // absolute or relative path, and need not refer to an existing node.
 // Parent directories of link are created as necessary.
+//
+// NOTE: Symlink is not supported by s3. It always returns an error.
 func (fs *S3FS) Symlink(target, link string) error {
 	return ErrSymLinkNotSupported
 }
 
 // Readlink returns the target path of link.
+//
+// NOTE: Readlink is not supported by s3. It always returns an error.
+// (This may be revised in the future.)
 func (fs *S3FS) Readlink(link string) (string, error) {
-	return "", nil
+	return "", ErrSymLinkNotSupported
 }
